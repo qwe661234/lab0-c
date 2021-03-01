bd3a790a6061f2e782b6833319529117a154bd3a
qwe661234 <qwe661234@gmail.com>
2021-03-02 00:19:14 +0800

    Bug fix: checking whether newh is NULL ot not in q_insert_head

--- queue.c
+++ queue.c
@@ -55,6 +55,9 @@ bool q_insert_head(queue_t *q, char *s)
         return false;
     }
     newh = malloc(sizeof(list_ele_t));
+    if (newh == NULL) {
+        return false;
+    }
     /* Don't forget to allocate space for the string and copy it */
     /* What if either call to malloc returns NULL? */
     int slen = strlen(s);
@@ -67,6 +70,7 @@ bool q_insert_head(queue_t *q, char *s)
     newh->value[slen] = '\0';
     newh->next = q->head;
     q->head = newh;
+    q->size++;
     return true;
 }
 
